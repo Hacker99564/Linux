4bee5e3aaaf2cc2d702ca0e880851e0f8e861107
Suryakant <121182957+Hacker99564@users.noreply.github.com>
2023-01-13 15:57:26 +0530

    ul.sh

---
+++ ul.sh
@@ -0,0 +1,10 @@
+apt update
+apt upgrade
+clear
+figlet Hello Hacker
+echo created by Suryakant
+echo Follow me on Instagram
+echo hacker_101_s
+echo Type fish which is help you in many ways.
+echo if its not working then  use apt install fish.
+sudo su
